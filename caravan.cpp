8e6642ff8aa2afc568ec0ebc9de2181fe5fdaeec
LucaTomala <luca.tomala@gmail.com>
2019-01-02 13:51:26 +0100

    some code

--- caravan.cpp
+++ caravan.cpp
@@ -10,16 +10,51 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <stdlib.h>
+#include "caravan.h"
+#include "general.h"
+
+#define COUNT_CARAVANS 5
+
+struct CaravanImplementation
+{
+  int length;
+  PackAnimal animal;
+  struct CaravanImplementation* next;
+};
+
+Caravan head = (Caravan)malloc(sizeof(struct CaravanImplementation));
+
 Caravan new_caravan()
 {
+    head->length = 0;
+    head->animal = 0;
+    head->next = 0;
+    return head;
 }
 
 int get_length(Caravan caravan)
 {
+  return head->length;
 }
 
 void delete_caravan(Caravan caravan)
 {
+  Caravan current = head;
+  while (current!=0)
+  {
+    if (caravan == head)
+    {
+      sfree(head);
+      return;
+    }
+    else if (caravan == current->next)
+    {
+      current->next = caravan->next;
+      sfree(caravan);
+      return;
+    }
+  }
 }
 
 void add_pack_animal(Caravan caravan, PackAnimal animal)
@@ -32,6 +67,7 @@ void remove_pack_animal(Caravan caravan, PackAnimal animal)
 
 int get_caravan_load(Caravan caravan)
 {
+  return 0;
 }
 
 void unload(Caravan caravan)
@@ -40,4 +76,5 @@ void unload(Caravan caravan)
 
 int get_caravan_speed(Caravan caravan)
 {
+  return 0;
 }
